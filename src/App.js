bebd1577a603c84274a29c13c4e5745d6e1fb04f
João Batista <jbsouzasantos1999@gmail.com>
2022-04-20 11:44:21 -0300

    Counter with useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const operation = {
+    add: () => {
+      setNumber(number + 1);
+    },
+    subtract: () => {
+      setNumber(number - 1);
+    },
+    reset: () => {
+      setNumber(0);
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="subcontainer number-container" onClick={operation.add}>
+        <h1> {number} </h1>
+      </div>
+      <div className="subcontainer btn-container">
+        <button className="add" onClick={operation.add}>
+          Add
+        </button>
+        <button className="sub" onClick={operation.subtract}>
+          Subtract
+        </button>
+        <button className="res" onClick={operation.reset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
